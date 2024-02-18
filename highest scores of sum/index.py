3ffbeb1f4678d62fec9f8dffe2cad6182e00e28e
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-18 18:40:55 +0530

    Update index.py

--- highest scores of sum/index.py
+++ highest scores of sum/index.py
@@ -1,1 +1,14 @@
 
+n=int(input())
+a=[]
+for i in range(n):
+    m=list(map(int,input().split(",")))
+    a.append(m)
+b=a[0]
+for i in a:
+    if sum(i)>sum(b):
+        b=i
+r=""
+for i in b:
+    r+=str(i)+" "
+print(r)
